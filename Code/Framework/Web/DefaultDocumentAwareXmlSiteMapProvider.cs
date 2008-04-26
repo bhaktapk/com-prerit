c1cedd2706cf6bcbb5c0a06eb9767baf91ac471d
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-04-26 14:16:37 +0000

    added a custom sitemap provider which handles the automatic resolving of a default document (i.e. default.aspx) to it's folder's sitemap node

---
+++ Code/Framework/Web/DefaultDocumentAwareXmlSiteMapProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+
+namespace Framework.Web
+{
+    public class DefaultDocumentAwareXmlSiteMapProvider : XmlSiteMapProvider
+    {
+        #region Methods
+
+        public override SiteMapNode FindSiteMapNode(string rawUrl)
+        {
+            SiteMapNode node = base.FindSiteMapNode(rawUrl);
+
+            if (node == null)
+            {
+                HttpContext context = HttpContext.Current;
+
+                if (context != null)
+                {
+                    string urlPath = context.Request.Path.ToLowerInvariant();
+
+                    int defaultDocumentIndex = urlPath.IndexOf("/default.aspx");
+
+                    if (defaultDocumentIndex != -1)
+                    {
+                        IncrementToIncludeTrailingSlash(ref defaultDocumentIndex);
+
+                        node = base.FindSiteMapNode(urlPath.Substring(0, defaultDocumentIndex));
+
+                        if (node != null && !node.IsAccessibleToUser(context))
+                        {
+                            node = null;
+                        }
+                    }
+                }
+            }
+
+            return node;
+        }
+
+        private void IncrementToIncludeTrailingSlash(ref int defaultDocumentIndex)
+        {
+            defaultDocumentIndex++;
+        }
+
+        #endregion
+    }
+}
