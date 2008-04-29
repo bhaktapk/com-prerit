a0ac835e3c99fcf43b42663cec06936edd7a492a
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-04-29 01:44:15 +0000

    svn move failed to move files and a bad commit was made

---
+++ trunk/Code/Com.Prerit.Web/Management/BufferedTextFileWebEventProviderMarkup.cs
@@ -0,0 +1,22 @@
+namespace Com.Prerit.Web.Management
+{
+    public sealed class BufferedTextFileWebEventProviderMarkup : StringEnum<BufferedTextFileWebEventProviderMarkup>
+    {
+        #region Fields
+
+        public static readonly BufferedTextFileWebEventProviderMarkup LogDirectoryPath = new BufferedTextFileWebEventProviderMarkup("logDirectoryPath");
+
+        public static readonly BufferedTextFileWebEventProviderMarkup LogFileFormat = new BufferedTextFileWebEventProviderMarkup("logFileNameFormat");
+
+        #endregion
+
+        #region Constructors
+
+        private BufferedTextFileWebEventProviderMarkup(string name)
+            : base(name)
+        {
+        }
+
+        #endregion
+    }
+}
