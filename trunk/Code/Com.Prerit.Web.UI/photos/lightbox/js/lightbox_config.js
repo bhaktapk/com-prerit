b758f9323e3aa6ba188a10813766d7ec871416bc
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-05-04 14:07:00 +0000

    added custom configuration of lightbox

---
+++ trunk/Code/Com.Prerit.Web.UI/photos/lightbox/js/lightbox_config.js
@@ -0,0 +1,3 @@
+﻿window.LightboxOptions.fileLoadingImage = 'lightbox/images/loading.gif';
+window.LightboxOptions.fileBottomNavCloseImage = 'lightbox/images/closelabel.gif';
+window.LightboxOptions.resizeSpeed = 8;
