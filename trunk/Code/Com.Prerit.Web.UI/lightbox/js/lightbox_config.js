5c9b4c7ca53c3c1f8e41ac8627dfcb168fd1d664
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-05-06 04:56:22 +0000

    setup website to act as the root app

--- trunk/Code/Com.Prerit.Web.UI/lightbox/js/lightbox_config.js
+++ trunk/Code/Com.Prerit.Web.UI/lightbox/js/lightbox_config.js
@@ -1,3 +1,3 @@
-﻿window.LightboxOptions.fileLoadingImage = 'lightbox/images/loading.gif';
-window.LightboxOptions.fileBottomNavCloseImage = 'lightbox/images/closelabel.gif';
+﻿window.LightboxOptions.fileLoadingImage = '/lightbox/images/loading.gif';
+window.LightboxOptions.fileBottomNavCloseImage = '/lightbox/images/closelabel.gif';
 window.LightboxOptions.resizeSpeed = 8;
