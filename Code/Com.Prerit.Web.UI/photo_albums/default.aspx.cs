a54a18178d880a5ad39247fe898d8e6d18d88981
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-05-07 00:07:23 +0000

    resized album cover photos because they did not fit into their CSS-specified container

--- Code/Com.Prerit.Web.UI/photo_albums/default.aspx.cs
+++ Code/Com.Prerit.Web.UI/photo_albums/default.aspx.cs
@@ -14,15 +14,15 @@ public partial class photo_albums_default : Page
         {
             { new List<Album>
                 {
-                    { new Album("Our First Home", 2007, "~/photo_albums/2007/our_first_house/", new Photo("example2.jpg", "~/photo_albums/2007/our_first_house/example2.jpg", 250, 187)) },
+                    { new Album("Our First Home", 2007, "~/photo_albums/2007/our_first_house/", new Photo("example2.jpg", "~/photo_albums/2007/our_first_house/example2.jpg", 224, 168)) },
                 }
             },
             { new List<Album>
                 {
-                    { new Album("Christmas", 2006, "~/photo_albums/2006/christmas/", new Photo("example1.jpg", "~/photo_albums/2006/christmas/example1.jpg", 187, 250)) },
-                    { new Album("Our Honeymoon", 2006, "~/photo_albums/2006/our_honeymoon/", new Photo("example2.jpg", "~/photo_albums/2006/our_honeymoon/example2.jpg", 250, 187)) },
-                    { new Album("Our Wedding", 2006, "~/photo_albums/2006/our_wedding/", new Photo("example3.jpg", "~/photo_albums/2006/our_wedding/example3.jpg", 187, 250)) },
-                    { new Album("P's Pithi", 2006, "~/photo_albums/2006/p's_pithi/", new Photo("example1.jpg", "~/photo_albums/2006/p's_pithi/example1.jpg", 187, 250)) },
+                    { new Album("Christmas", 2006, "~/photo_albums/2006/christmas/", new Photo("example1.jpg", "~/photo_albums/2006/christmas/example1.jpg", 168, 224)) },
+                    { new Album("Our Honeymoon", 2006, "~/photo_albums/2006/our_honeymoon/", new Photo("example2.jpg", "~/photo_albums/2006/our_honeymoon/example2.jpg", 224, 168)) },
+                    { new Album("Our Wedding", 2006, "~/photo_albums/2006/our_wedding/", new Photo("example3.jpg", "~/photo_albums/2006/our_wedding/example3.jpg", 168, 224)) },
+                    { new Album("P's Pithi", 2006, "~/photo_albums/2006/p's_pithi/", new Photo("example1.jpg", "~/photo_albums/2006/p's_pithi/example1.jpg", 168, 224)) },
                 }
             }
         };
