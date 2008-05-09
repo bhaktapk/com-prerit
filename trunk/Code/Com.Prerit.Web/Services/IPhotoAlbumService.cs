dc241a05869e3bfc261f26680f256c969a7f094e
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-05-09 05:48:20 +0000

    added task item to rename interface method names

--- trunk/Code/Com.Prerit.Web/Services/IPhotoAlbumService.cs
+++ trunk/Code/Com.Prerit.Web/Services/IPhotoAlbumService.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 
 namespace Com.Prerit.Web.Services
 {
+    // TODO: change method names from "get" to "find" and return null if not found
     public interface IPhotoAlbumService
     {
         #region Methods
