0de2f38ec0a7636c026121b50b14349a0db97d3b
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-05-15 01:48:50 +0000

    added AssociatedThumbnail property on Photo class
    partially implemented getting the associatedThumbnail for each photo
    
    photo page now displays the associated thumbnail

--- trunk/Code/Com.Prerit.Web/Photo.cs
+++ trunk/Code/Com.Prerit.Web/Photo.cs
@@ -1,12 +1,26 @@
+using System;
+
 namespace Com.Prerit.Web
 {
     public class Photo : WebImage
     {
         #region Constructors
 
-        public Photo(string caption, string virtualPath, int height, int width)
+        public WebImage AssociatedThumbnail
+        {
+            get;
+            private set;
+        }
+
+        public Photo(string caption, string virtualPath, int height, int width, WebImage associatedThumbnail)
             : base(caption, virtualPath, height, width)
         {
+            if (associatedThumbnail == null)
+            {
+                throw new ArgumentNullException("associatedThumbnail");
+            }
+
+            AssociatedThumbnail = associatedThumbnail;
         }
 
         #endregion
