b60a9de23576a5261e87a5eb1de599e78c35b556
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-05-24 20:49:46 +0000

    abstracted cache dependencies to a separate class
    added more robust trace information

---
+++ Code/Com.Prerit.Web/CacheItemDependency.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
+
+namespace Com.Prerit.Web
+{
+    public static class CacheItemDependency
+    {
+        #region Methods
+
+        public static CacheDependency GetAlbumYearsDependency(AlbumYear[] albumYears, string photoAlbumsVirtualPath)
+        {
+            if (albumYears == null)
+            {
+                throw new ArgumentNullException("albumYears");
+            }
+
+            if (!VirtualPathUtility.IsAbsolute(photoAlbumsVirtualPath) && !VirtualPathUtility.IsAppRelative(photoAlbumsVirtualPath))
+            {
+                throw new ArgumentException("Path must either be an absolute virtual path or app relative virtual path", "photoAlbumsVirtualPath");
+            }
+
+            CacheDependency result;
+
+            List<string> folderDependencyList = new List<string>();
+
+            folderDependencyList.Add(HostingEnvironment.MapPath(photoAlbumsVirtualPath));
+
+            foreach (AlbumYear albumYear in albumYears)
+            {
+                folderDependencyList.Add(HostingEnvironment.MapPath(albumYear.VirtualPath));
+
+                Debug.Assert(albumYear.Albums != null);
+
+                foreach (Album album in albumYear.Albums)
+                {
+                    folderDependencyList.Add(HostingEnvironment.MapPath(album.VirtualPath));
+                }
+            }
+
+            result = new CacheDependency(folderDependencyList.ToArray());
+
+            return result;
+        }
+
+        #endregion
+    }
+}
