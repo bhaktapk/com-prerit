66573e93034da26b7177b2e3e6d993f5a37523a5
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2008-05-26 17:46:05 +0000

    simplified code by using lambda expressions

--- trunk/Code/Com.Prerit.Services/AsyncCacheItemLoaderService.cs
+++ trunk/Code/Com.Prerit.Services/AsyncCacheItemLoaderService.cs
@@ -7,21 +7,6 @@ namespace Com.Prerit.Services
     {
         #region Methods
 
-        private void LoadAndSetCacheItem<T>(ILoaderService<T> loaderService, Action<T> cacheItemSetter)
-        {
-            Debug.Assert(loaderService != null);
-            Debug.Assert(cacheItemSetter != null);
-
-            try
-            {
-                cacheItemSetter(loaderService.Load());
-            }
-            catch (Exception e)
-            {
-                Trace.TraceError("Async cache item loader error:{0}{0}{1}", Environment.NewLine, e);
-            }
-        }
-
         public IAsyncResult LoadAsync<T>(ILoaderService<T> loaderService, Action<T> cacheItemSetter)
         {
             if (loaderService == null)
@@ -34,9 +19,9 @@ namespace Com.Prerit.Services
                 throw new ArgumentNullException("cacheItemSetter");
             }
 
-            Action<ILoaderService<T>, Action<T>> loadAndSetCacheItem = LoadAndSetCacheItem;
+            Action loadAndSetCacheItem = () => cacheItemSetter(loaderService.Load());
 
-            return loadAndSetCacheItem.BeginInvoke(loaderService, cacheItemSetter, loadAndSetCacheItem.EndInvoke, null);
+            return loadAndSetCacheItem.BeginInvoke(loadAndSetCacheItem.EndInvoke, null);
         }
 
         #endregion
