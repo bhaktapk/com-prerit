d6cda93758f25cfd60dbc21c262ce3728cec0a71
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-03-28 17:05:31 +0000

    split AssemblyInfo information into 2 files: one common and one project specific

---
+++ Code/Com.Prerit.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly : AssemblyTitle("Com.Prerit.Core")]
+[assembly : AssemblyProduct("Com.Prerit.Core")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly : Guid("b303be6d-702d-4cfd-9a60-0f74b517ef18")]
