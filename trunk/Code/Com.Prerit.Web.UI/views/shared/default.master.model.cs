55125429fa03c774f28c09cc9651a89b7ca8e6bf
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-03 22:09:38 +0000

    removed models folder; testing out new organizational pattern for strongly typed view data
    added initial code for master page

---
+++ trunk/Code/Com.Prerit.Web.UI/views/shared/default.master.model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Com.Prerit.Web.UI.Views.Shared
+{
+    public abstract class DefaultMasterModel
+    {
+        #region Properties
+
+        public int CopyrightBeginYear { get; set; }
+
+        public int CopyrightEndYear { get; set; }
+
+        public string SiteName { get; set; }
+
+        public Uri ValidationUri { get; set; }
+
+        public string WebsiteAuthor { get; set; }
+
+        #endregion
+    }
+}
