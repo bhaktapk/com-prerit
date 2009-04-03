e562e8aacc7d92fc3955071d7e4044205d280ce5
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-03 22:48:15 +0000

    created initial master controller

---
+++ trunk/Code/Com.Prerit.Web.UI/controllers/DefaultMasterController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Web.Mvc;
+
+using Com.Prerit.Web.UI.Views.Shared;
+
+namespace Com.Prerit.Web.UI.controllers
+{
+    public abstract class DefaultMasterController<T> : Controller where T : DefaultMasterModel, new()
+    {
+        #region Methods
+
+        public T CreateBaseModel()
+        {
+            {
+                return new T
+                           {
+                               CopyrightBeginYear = WebsiteInfo.DomainRegistrationYear,
+                               CopyrightEndYear = DateTime.Today.Year,
+                               SiteName = WebsiteInfo.SiteName,
+                               ValidationUri = Request.Url,
+                               WebsiteAuthor = WebsiteInfo.Author
+                           };
+            }
+        }
+
+        #endregion
+    }
+}
