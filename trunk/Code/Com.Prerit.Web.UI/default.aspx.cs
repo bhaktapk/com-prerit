1dacc0d03bd46561aef06da058483d2a0feb47b1
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-04 12:44:30 +0000

    cleaned up stock asp.net mvc code

--- trunk/Code/Com.Prerit.Web.UI/default.aspx.cs
+++ trunk/Code/Com.Prerit.Web.UI/default.aspx.cs
@@ -1,22 +1,23 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.UI;
 
 namespace Com.Prerit.Web.UI
 {
-    public partial class _Default : Page
+    public class _Default : Page
     {
-        public void Page_Load(object sender, System.EventArgs e)
-        {
-            // Change the current path so that the Routing handler can correctly interpret
-            // the request, then restore the original path so that the OutputCache module
-            // can correctly process the response (if caching is enabled).
+        #region Methods
 
+        public void Page_Load(object sender, EventArgs e)
+        {
             string originalPath = Request.Path;
             HttpContext.Current.RewritePath(Request.ApplicationPath, false);
             IHttpHandler httpHandler = new MvcHttpHandler();
             httpHandler.ProcessRequest(HttpContext.Current);
             HttpContext.Current.RewritePath(originalPath, false);
         }
+
+        #endregion
     }
 }
