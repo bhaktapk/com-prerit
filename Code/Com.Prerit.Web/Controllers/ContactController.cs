5dcc024dafb15245cd3d17e1474a41fa45449497
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-11 14:48:13 +0000

    implemented mode, view, controller for contact page
    changed formError to default mvc input error class (i.e. input-validation-error)

--- Code/Com.Prerit.Web/Controllers/ContactController.cs
+++ Code/Com.Prerit.Web/Controllers/ContactController.cs
@@ -1,3 +1,5 @@
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 using Com.Prerit.Web.Models.Contact;
@@ -12,7 +14,54 @@ namespace Com.Prerit.Web.Controllers
         [ActionName(Action.Index)]
         public ActionResult Index()
         {
-            var model = UpdateModelBase(new IndexModel());
+            IndexModel model = UpdateModelBase(new IndexModel());
+
+            return View(model);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        [ActionName(Action.Index)]
+        public ActionResult Index(IndexModel model)
+        {
+            UpdateModelBase(model);
+
+            if (model.Name.Trim().Length == 0)
+            {
+                ModelState.AddModelError(IndexModel.PropertyName.Name, "Name is required");
+            }
+
+            if (model.EmailAddress.Trim().Length == 0)
+            {
+                ModelState.AddModelError(IndexModel.PropertyName.EmailAddress, "E-mail is required");
+            }
+            else if (!Regex.IsMatch(model.EmailAddress, @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"))
+            {
+                ModelState.AddModelError(IndexModel.PropertyName.EmailAddress, "E-mail is not in a correct format");
+            }
+
+            if (model.Message.Trim().Length == 0)
+            {
+                ModelState.AddModelError(IndexModel.PropertyName.Message, "Message is required");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var smtpClient = new SmtpClient
+                                     {
+                                         Host = WebsiteInfo.SmtpHost
+                                     };
+
+                using (var message = new MailMessage())
+                {
+                    message.From = new MailAddress(WebsiteInfo.AuthorEmailAddress);
+                    message.To.Add(model.EmailAddress);
+                    message.Subject = WebsiteInfo.GetContactEmailSubject(model.Name);
+                    message.Body = model.Message;
+                    message.IsBodyHtml = false;
+
+                    smtpClient.Send(message);
+                }
+            }
 
             return View(model);
         }
