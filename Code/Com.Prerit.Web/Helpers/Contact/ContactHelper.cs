bf568307d81a512a79cdaf31ad76db16c5e9561e
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-11 14:45:39 +0000

    removed CssClassSelector
    added helpers for contact page

---
+++ Code/Com.Prerit.Web/Helpers/Contact/ContactHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Com.Prerit.Web.Helpers.Contact
+{
+    public static class ContactHelper
+    {
+        #region Nested Type: PartialName
+
+        public static class PartialName
+        {
+            #region Fields
+
+            public static readonly string ValidationSummary = "validation-summary";
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
