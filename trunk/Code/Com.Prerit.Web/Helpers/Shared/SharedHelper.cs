80c6eb09f93ba7a57aad02ee76e1f03b0d5b5b08
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-11 20:16:31 +0000

    implemented mode, view, controller for sent email page

---
+++ trunk/Code/Com.Prerit.Web/Helpers/Shared/SharedHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Com.Prerit.Web.Helpers.Shared
+{
+    public static class SharedHelper
+    {
+        #region Methods
+
+        public static string HtmlizeLineBreaks(this HtmlHelper helper, string s)
+        {
+            return s.Replace(Environment.NewLine, "<br />");
+        }
+
+        #endregion
+    }
+}
