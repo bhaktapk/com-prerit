1ab6b535a9d57032e2e9081efbaa9ddcf1d97ad5
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-11 14:45:39 +0000

    removed CssClassSelector
    added helpers for contact page

---
+++ trunk/Code/Com.Prerit.Web/Helpers/Contact/Index/ContactIndexHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Com.Prerit.Web.Helpers.Contact.Index
+{
+    public static class ContactIndexHelper
+    {
+        #region Methods
+
+        public static bool IsValidationSummaryVisible(this HtmlHelper helper)
+        {
+            return !helper.ViewData.ModelState.IsValid;
+        }
+
+        #endregion
+    }
+}
