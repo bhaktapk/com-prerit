cdab972fb1c42af8d80b7575c3132e8ff1065f34
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-04-11 14:48:13 +0000

    implemented mode, view, controller for contact page
    changed formError to default mvc input error class (i.e. input-validation-error)

--- trunk/Code/Com.Prerit.Web/Models/Contact/IndexModel.cs
+++ trunk/Code/Com.Prerit.Web/Models/Contact/IndexModel.cs
@@ -1,8 +1,38 @@
-﻿using Com.Prerit.Web.Models.Shared;
+﻿using System.Web.Mvc;
+
+using Com.Prerit.Core;
+using Com.Prerit.Web.Models.Shared;
 
 namespace Com.Prerit.Web.Models.Contact
 {
+    [Bind]
     public class IndexModel : DefaultMasterModel
     {
+        #region Properties
+
+        public string EmailAddress { get; set; }
+
+        public string Message { get; set; }
+
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Nested Type: PropertyName
+
+        public static class PropertyName
+        {
+            #region Fields
+
+            public static readonly string EmailAddress = TypeUtil.GetPropertyName<IndexModel, string>(m => m.EmailAddress).ToLowerFirstLetter();
+
+            public static readonly string Message = TypeUtil.GetPropertyName<IndexModel, string>(m => m.Message).ToLowerFirstLetter();
+
+            public static readonly string Name = TypeUtil.GetPropertyName<IndexModel, string>(m => m.Name).ToLowerFirstLetter();
+
+            #endregion
+        }
+
+        #endregion
     }
 }
