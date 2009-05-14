f9d7548857c61ae4bd83c2c4bb4f27c9f9a95622
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-05-14 12:51:39 +0000

    converted double foreach loop in ASPX template into a code base repeater

---
+++ Code/Com.Prerit.Web/Helpers/Contact/Index/ContactValidationSummaryHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Com.Prerit.Web.Helpers.Contact.Index
+{
+    public static class ContactValidationSummaryHelper
+    {
+        #region Methods
+
+        public static void RepeatErrorMessages(this HtmlHelper helper, Action<ModelError> render)
+        {
+            foreach (ModelState modelState in helper.ViewData.ModelState.Values)
+            {
+                foreach (ModelError error in modelState.Errors)
+                {
+                    render(error);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
