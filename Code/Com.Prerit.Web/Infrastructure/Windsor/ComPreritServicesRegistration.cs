4966ded612e6b6580fc680ce68265ecf39ad7300
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-05-15 00:46:43 +0000

    modified WindsorContainerInitializer to be a bootstrapper of a bootstrapper in order to make Windsor registration modular

---
+++ Code/Com.Prerit.Web/Infrastructure/Windsor/ComPreritServicesRegistration.cs
@@ -0,0 +1,22 @@
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+
+using Com.Prerit.Services;
+
+namespace Com.Prerit.Web.Infrastructure.Windsor
+{
+    public class ComPreritServicesRegistration : IRegistration
+    {
+        public void Register(IKernel kernel)
+        {
+            const string assemblyName = "Com.Prerit.Services";
+
+            kernel
+                .Register(
+                    AllTypes.Pick().FromAssemblyNamed(assemblyName)
+                    .Configure(c => c.LifeStyle.Transient)
+                    .ConfigureFor<IEmailSenderService>(c => c.Parameters(Parameter.ForKey("smtpHost").Eq(EmailInfo.SmtpHost)))
+                    .WithService.FirstInterface());
+        }
+    }
+}
