9aca54c5d94edcc7b4108af5df03d1294fde5547
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-05-16 03:17:42 +0000

    added nunit
    added unit testing project stubs

---
+++ trunk/Code/Com.Prerit.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly : AssemblyTitle("Com.Prerit.Web.Tests")]
+[assembly : AssemblyProduct("Com.Prerit.Web.Tests")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly : Guid("80bf269f-8fca-4649-a23f-165c86bf64be")]
