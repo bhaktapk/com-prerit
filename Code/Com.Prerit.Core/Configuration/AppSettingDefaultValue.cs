f0582cf75bb311b14299d992e75a0cdec1b4f71e
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-05-16 19:17:23 +0000

    abstracted appSetting default values

---
+++ Code/Com.Prerit.Core/Configuration/AppSettingDefaultValue.cs
@@ -0,0 +1,36 @@
+﻿namespace Com.Prerit.Core.Configuration
+{
+    public class AppSettingDefaultValue<T>
+    {
+        #region Properties
+
+        public static AppSettingDefaultValue<bool> CacheAlbumPhotos { get; private set; }
+
+        public T DefaultValue { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        static AppSettingDefaultValue()
+        {
+            CacheAlbumPhotos = new AppSettingDefaultValue<bool>(true);
+        }
+
+        protected AppSettingDefaultValue(T defaultValue)
+        {
+            DefaultValue = defaultValue;
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static implicit operator T(AppSettingDefaultValue<T> appSettingDefaultValue)
+        {
+            return appSettingDefaultValue.DefaultValue;
+        }
+
+        #endregion
+    }
+}
