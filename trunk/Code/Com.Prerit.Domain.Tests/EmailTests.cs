a36880c733cee9d24eb54967188a0e2ef3b0c296
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-15 20:05:05 +0000

    added test project for domain layer

---
+++ trunk/Code/Com.Prerit.Domain.Tests/EmailTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using Castle.Components.Validator;
+
+using Com.Prerit.Core;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Domain.Tests
+{
+    [TestFixture]
+    public class EmailTests
+    {
+        #region Tests
+
+        [Test]
+        public void ShouldContainValidateEmailAttributeOnFromEmailAddressProperty()
+        {
+            object[] attributes = GetAttributes(e => e.FromEmailAddress, typeof(ValidateEmailAttribute));
+
+            Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldContainValidateEmailAttributeOnToEmailAddressProperty()
+        {
+            object[] attributes = GetAttributes(e => e.ToEmailAddress, typeof(ValidateEmailAttribute));
+
+            Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldContainValidateNonEmptyAttributeOnFromEmailAddressProperty()
+        {
+            object[] attributes = GetAttributes(e => e.FromEmailAddress, typeof(ValidateNonEmptyAttribute));
+
+            Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldContainValidateNonEmptyAttributeOnMessageProperty()
+        {
+            object[] attributes = GetAttributes(e => e.Message, typeof(ValidateNonEmptyAttribute));
+
+            Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldContainValidateNonEmptyAttributeOnSubjectProperty()
+        {
+            object[] attributes = GetAttributes(e => e.Subject, typeof(ValidateNonEmptyAttribute));
+
+            Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ShouldContainValidateNonEmptyAttributeOnToEmailAddressProperty()
+        {
+            object[] attributes = GetAttributes(e => e.ToEmailAddress, typeof(ValidateNonEmptyAttribute));
+
+            Assert.That(attributes.Length, Is.EqualTo(1));
+        }
+
+        #endregion
+
+        #region Methods
+
+        private object[] GetAttributes(Expression<Func<Email, object>> member, Type attributeType)
+        {
+            return typeof(Email).GetProperty(TypeUtil.GetMemberName(member)).GetCustomAttributes(attributeType, false);
+        }
+
+        #endregion
+    }
+}
