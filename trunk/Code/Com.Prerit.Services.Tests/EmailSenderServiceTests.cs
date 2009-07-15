8b630c489ce802d7cba8111fa7d1e2610f314bb6
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-15 20:49:23 +0000

    added unit test for EmailSenderService

---
+++ trunk/Code/Com.Prerit.Services.Tests/EmailSenderServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Castle.Components.Validator;
+
+using Com.Prerit.Domain;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Services.Tests
+{
+    [TestFixture]
+    public class EmailSenderServiceTests
+    {
+        #region Fields
+
+        private EmailSenderService _emailSenderService;
+
+        #endregion
+
+        #region Setup/Teardown
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _emailSenderService = new EmailSenderService("localhost", new ValidatorRunner(new CachedValidationRegistry()));
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void ShouldFailToSendEmailBecauseEmailIsInvalid()
+        {
+            var email = new Email();
+
+            Assert.That(() => _emailSenderService.Send(email), Throws.InstanceOf<ValidationException>());
+        }
+
+        #endregion
+    }
+}
