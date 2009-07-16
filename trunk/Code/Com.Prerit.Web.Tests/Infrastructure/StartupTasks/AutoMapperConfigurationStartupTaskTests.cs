977bc3509d90e364c67c2199850a8a0c02266a8f
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-16 16:18:21 +0000

    added tests for AutoMapperConfigurationStartupTask

---
+++ trunk/Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/AutoMapperConfigurationStartupTaskTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+
+using Com.Prerit.Web.Infrastructure.StartupTasks;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Web.Tests.Infrastructure.StartupTasks
+{
+    [TestFixture]
+    public class AutoMapperConfigurationStartupTaskTests
+    {
+        #region Setup/Teardown
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            StartupTaskRunner.Run();
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Should_Assert_Configuration_Is_Valid()
+        {
+            Mapper.CreateMap<Source, Destination>();
+
+            Assert.That(() => new AutoMapperConfigurationStartupTask().Execute(), Throws.InstanceOf<AutoMapperConfigurationException>());
+        }
+
+        [Test]
+        public void Should_Contain_Maps()
+        {
+            new AutoMapperConfigurationStartupTask().Execute();
+
+            Assert.That(Mapper.GetAllTypeMaps(), Is.Not.Empty);
+        }
+
+        #endregion
+
+        private class Source
+        {
+            public int SomeValue { get; set; }
+        }
+
+        private class Destination
+        {
+            public int SomeValuefff { get; set; }
+        }
+
+    }
+}
