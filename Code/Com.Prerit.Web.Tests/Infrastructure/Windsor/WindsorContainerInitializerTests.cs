ef827d3731bd48a53001552756daf3bdc6fac6ba
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-16 14:40:59 +0000

    added tests for WindsorContainerInitializer

---
+++ Code/Com.Prerit.Web.Tests/Infrastructure/Windsor/WindsorContainerInitializerTests.cs
@@ -0,0 +1,25 @@
+﻿using Castle.Windsor;
+
+using Com.Prerit.Web.Infrastructure.StartupTasks;
+using Com.Prerit.Web.Infrastructure.Windsor;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Web.Tests.Infrastructure.Windsor
+{
+    [TestFixture]
+    public class WindsorContainerInitializerTests
+    {
+        #region Tests
+
+        [Test]
+        public void Should_Initialize_Windsor_Container()
+        {
+            IWindsorContainer container = WindsorContainerInitializer.Init();
+
+            Assert.That(container.ResolveAll<IStartupTask>(), Is.Not.Empty);
+        }
+
+        #endregion
+    }
+}
