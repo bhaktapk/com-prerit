b836c84ee586e51577711a94e6fd16ae8790c36c
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-16 18:53:15 +0000

    simplified RegisterDefaultModelBinderStartupTask
    added tests for RegisterDefaultModelBinderStartupTask

--- trunk/Code/Com.Prerit.Web/Infrastructure/StartupTasks/RegisterDefaultModelBinderStartupTask.cs
+++ trunk/Code/Com.Prerit.Web/Infrastructure/StartupTasks/RegisterDefaultModelBinderStartupTask.cs
@@ -1,56 +1,23 @@
-using System;
 using System.Web.Mvc;
 
-using Castle.Components.Validator;
-
 using Com.Prerit.Web.Infrastructure.ModelBinders;
+
+using Microsoft.Practices.ServiceLocation;
 
 namespace Com.Prerit.Web.Infrastructure.StartupTasks
 {
     public class RegisterDefaultModelBinderStartupTask : IStartupTask
     {
-        #region Fields
-
-        private readonly SimpleValidatingModelBinder _modelBinder;
-
-        private readonly ModelBinderDictionary _modelBinderDictionary;
-
-        #endregion
-
-        #region Constructors
-
-        public RegisterDefaultModelBinderStartupTask(ModelBinderDictionary modelBinderDictionary, SimpleValidatingModelBinder modelBinder)
-        {
-            if (modelBinderDictionary == null)
-            {
-                throw new ArgumentNullException("modelBinderDictionary");
-            }
-
-            if (modelBinder == null)
-            {
-                throw new ArgumentNullException("modelBinder");
-            }
-
-            _modelBinderDictionary = modelBinderDictionary;
-            _modelBinder = modelBinder;
-        }
-
-        public RegisterDefaultModelBinderStartupTask()
-            : this(System.Web.Mvc.ModelBinders.Binders, new SimpleValidatingModelBinder(new ValidatorRunner(new CachedValidationRegistry())))
-        {
-        }
-
-        #endregion
-
         #region Methods
 
         public void Execute()
         {
-            _modelBinderDictionary.DefaultBinder = _modelBinder;
+            System.Web.Mvc.ModelBinders.Binders.DefaultBinder = ServiceLocator.Current.GetInstance<SimpleValidatingModelBinder>();
         }
 
         public void Reset()
         {
+            System.Web.Mvc.ModelBinders.Binders.DefaultBinder = new DefaultModelBinder();
         }
 
         #endregion
