9624e9a52ce1df6e32e8084fc674f93c47d407bc
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-16 18:53:15 +0000

    simplified RegisterDefaultModelBinderStartupTask
    added tests for RegisterDefaultModelBinderStartupTask

---
+++ Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/RegisterDefaultModelBinderStartupTaskTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+
+using Com.Prerit.Web.Infrastructure.ModelBinders;
+using Com.Prerit.Web.Infrastructure.StartupTasks;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Web.Tests.Infrastructure.StartupTasks
+{
+    [TestFixture]
+    public class RegisterDefaultModelBinderStartupTaskTests
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            StartupTaskRunner.Run();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            StartupTaskRunner.Reset();
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Should_Reset_Binder()
+        {
+            var task = new RegisterDefaultModelBinderStartupTask();
+
+            task.Execute();
+            task.Reset();
+
+            Assert.That(ModelBinders.Binders.DefaultBinder, Is.InstanceOf<DefaultModelBinder>());
+        }
+
+        [Test]
+        public void Should_Set_Default_Binder()
+        {
+            new RegisterDefaultModelBinderStartupTask().Execute();
+
+            Assert.That(ModelBinders.Binders.DefaultBinder, Is.InstanceOf<SimpleValidatingModelBinder>());
+        }
+
+        #endregion
+    }
+}
