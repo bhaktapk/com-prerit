e590427c48e7fbeae26718160e938346de61755a
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-16 14:27:54 +0000

    added tests for StartupTaskRunner

---
+++ Code/Com.Prerit.Web.Tests/Infrastructure/StartupTaskRunnerTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+
+using Com.Prerit.Web.Infrastructure.StartupTasks;
+using Com.Prerit.Web.Infrastructure.Windsor;
+
+using CommonServiceLocator.WindsorAdapter;
+
+using Microsoft.Practices.ServiceLocation;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Web.Tests.Infrastructure
+{
+    [TestFixture]
+    public class StartupTaskRunnerTests
+    {
+        #region Setup/Teardown
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            typeof(StartupTaskRunner).TypeInitializer.Invoke(null, new object[0]);
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Should_Configure_ControllerFactory()
+        {
+            Assert.That(ControllerBuilder.Current.GetControllerFactory(), Is.TypeOf<WindsorControllerFactory>());
+        }
+
+        [Test]
+        public void Should_Configure_ServiceLocator()
+        {
+            Assert.That(ServiceLocator.Current, Is.TypeOf<WindsorServiceLocator>());
+        }
+
+        [Test]
+        public void Should_Run_StartupTasks()
+        {
+            Assert.That(() => StartupTaskRunner.Run(), Throws.Nothing);
+        }
+
+        #endregion
+    }
+}
