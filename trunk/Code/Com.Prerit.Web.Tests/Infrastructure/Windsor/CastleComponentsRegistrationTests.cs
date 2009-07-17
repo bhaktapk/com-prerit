13d96d1daf8d410090a07fe87469511d339afc8e
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-07-17 14:15:24 +0000

    added tests for CastleComponentsRegistration

---
+++ trunk/Code/Com.Prerit.Web.Tests/Infrastructure/Windsor/CastleComponentsRegistrationTests.cs
@@ -0,0 +1,28 @@
+﻿using Castle.Components.Validator;
+using Castle.Windsor;
+
+using Com.Prerit.Web.Infrastructure.Windsor;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Web.Tests.Infrastructure.Windsor
+{
+    [TestFixture]
+    public class CastleComponentsRegistrationTests
+    {
+        #region Tests
+
+        [Test]
+        public void Should_Register_Services()
+        {
+            using (var container = new WindsorContainer())
+            {
+                container.Register(new CastleComponentsRegistration());
+
+                Assert.That(container.Resolve(typeof(IValidator)), Is.Not.Null);
+            }
+        }
+
+        #endregion
+    }
+}
