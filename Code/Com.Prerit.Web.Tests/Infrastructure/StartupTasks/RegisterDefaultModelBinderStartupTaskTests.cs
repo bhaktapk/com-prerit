2b84f4c692822ba4ad939e97b22356c65a0ee400
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-10-21 00:01:39 +0000

    rewrote unit tests in AAA syntax

--- Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/RegisterDefaultModelBinderStartupTaskTests.cs
+++ Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/RegisterDefaultModelBinderStartupTaskTests.cs
@@ -1,7 +1,13 @@
 ﻿using System.Web.Mvc;
 
+using Castle.Components.Validator;
+
 using Com.Prerit.Web.Infrastructure.ModelBinders;
 using Com.Prerit.Web.Infrastructure.StartupTasks;
+
+using Microsoft.Practices.ServiceLocation;
+
+using Moq;
 
 using NUnit.Framework;
 
@@ -10,41 +16,42 @@ namespace Com.Prerit.Web.Tests.Infrastructure.StartupTasks
     [TestFixture]
     public class RegisterDefaultModelBinderStartupTaskTests
     {
-        #region Setup/Teardown
-
-        [SetUp]
-        public void SetUp()
-        {
-            StartupTaskRunner.Run();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            StartupTaskRunner.Reset();
-        }
-
-        #endregion
-
         #region Tests
 
         [Test]
         public void Should_Reset_Binder()
         {
-            var task = new RegisterDefaultModelBinderStartupTask();
+            // arrange
+            ModelBinders.Binders.DefaultBinder = null;
 
-            task.Execute();
-            task.Reset();
+            // act
+            new RegisterDefaultModelBinderStartupTask().Reset();
 
+            // assert
             Assert.That(ModelBinders.Binders.DefaultBinder, Is.InstanceOf<DefaultModelBinder>());
         }
 
         [Test]
         public void Should_Set_Default_Binder()
         {
+            // arrange
+            var serviceLocator = new Mock<IServiceLocator>();
+            var runner = new Mock<IValidatorRunner>();
+            var simpleValidatingModelBinder = new Mock<SimpleValidatingModelBinder>(runner.Object);
+
+            serviceLocator.Setup(sl => sl.GetInstance<SimpleValidatingModelBinder>()).Returns(simpleValidatingModelBinder.Object);
+
+            ServiceLocator.SetLocatorProvider(() => serviceLocator.Object);
+
+            // act
             new RegisterDefaultModelBinderStartupTask().Execute();
 
+            // assert
             Assert.That(ModelBinders.Binders.DefaultBinder, Is.InstanceOf<SimpleValidatingModelBinder>());
+
+            // teardown
+            ModelBinders.Binders.DefaultBinder = new DefaultModelBinder();
+            ServiceLocator.SetLocatorProvider(() => null);
         }
 
         #endregion
