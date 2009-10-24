a0d9e159152812451e71836d9e58a5caff6e3dcd
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-10-24 12:42:02 +0000

    added unit test for StartupTaskRunner.Run
    reformatted code

--- trunk/Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/StartupTaskRunnerTests.cs
+++ trunk/Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/StartupTaskRunnerTests.cs
@@ -36,6 +36,37 @@ namespace Com.Prerit.Web.Tests.Infrastructure.StartupTasks
         }
 
         [Test]
+        public void Should_Configure_ServiceLocator()
+        {
+            // arrange
+            var container = new Mock<IWindsorContainer>();
+
+            container.Setup(c => c.ResolveAll(typeof(IStartupTask))).Returns(new IStartupTask[] { });
+
+            // act
+            StartupTaskRunner.Run(container.Object);
+
+            // assert
+            Assert.That(ServiceLocator.Current, Is.TypeOf<WindsorServiceLocator>());
+        }
+
+        [Test]
+        public void Should_Execute_StartupTasks()
+        {
+            // arrange
+            var container = new Mock<IWindsorContainer>();
+            var startupTask = new Mock<IStartupTask>();
+
+            container.Setup(c => c.ResolveAll(typeof(IStartupTask))).Returns(new[] { startupTask.Object, startupTask.Object });
+
+            // act
+            StartupTaskRunner.Run(container.Object);
+
+            // assert
+            startupTask.Verify(task => task.Execute(), Times.Exactly(2));
+        }
+
+        [Test]
         public void Should_Reset_ControllerFactory()
         {
             // arrange
@@ -70,37 +101,6 @@ namespace Com.Prerit.Web.Tests.Infrastructure.StartupTasks
         }
 
         [Test]
-        public void Should_Configure_ServiceLocator()
-        {
-            // arrange
-            var container = new Mock<IWindsorContainer>();
-
-            container.Setup(c => c.ResolveAll(typeof(IStartupTask))).Returns(new IStartupTask[] { });
-
-            // act
-            StartupTaskRunner.Run(container.Object);
-
-            // assert
-            Assert.That(ServiceLocator.Current, Is.TypeOf<WindsorServiceLocator>());
-        }
-
-        [Test]
-        public void Should_Execute_StartupTasks()
-        {
-            // arrange
-            var container = new Mock<IWindsorContainer>();
-            var startupTask = new Mock<IStartupTask>();
-
-            container.Setup(c => c.ResolveAll(typeof(IStartupTask))).Returns(new[] { startupTask.Object, startupTask.Object });
-
-            // act
-            StartupTaskRunner.Run(container.Object);
-
-            // assert
-            startupTask.Verify(task => task.Execute(), Times.Exactly(2));
-        }
-
-        [Test]
         public void Should_Reset_StartupTasks()
         {
             // arrange
@@ -116,6 +116,13 @@ namespace Com.Prerit.Web.Tests.Infrastructure.StartupTasks
 
             // assert
             startupTask.Verify(task => task.Reset(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void Should_Run_With_Auto_Configuration()
+        {
+            // act
+            StartupTaskRunner.Run();
         }
 
         #endregion
