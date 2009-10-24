641ad3363175a4528ba5ea959d9fae2ff56aabe4
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-10-24 03:42:55 +0000

    updated test to AAA test style
    utilize Moq for mocking
    
    restructured StartupTaskRunner for easier testing
    
    changed Reset of StartupTaskRunner so that ServiceLocator and ControllerFactory are reset as well

--- trunk/Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/StartupTaskRunnerTests.cs
+++ trunk/Code/Com.Prerit.Web.Tests/Infrastructure/StartupTasks/StartupTaskRunnerTests.cs
@@ -1,11 +1,15 @@
 ﻿using System.Web.Mvc;
 
+using Castle.Windsor;
+
 using Com.Prerit.Web.Infrastructure.StartupTasks;
 using Com.Prerit.Web.Infrastructure.Windsor;
 
 using CommonServiceLocator.WindsorAdapter;
 
 using Microsoft.Practices.ServiceLocation;
+
+using Moq;
 
 using NUnit.Framework;
 
@@ -14,55 +18,104 @@ namespace Com.Prerit.Web.Tests.Infrastructure.StartupTasks
     [TestFixture]
     public class StartupTaskRunnerTests
     {
-        #region Setup/Teardown
-
-        [TestFixtureSetUp]
-        public void TestFixtureSetUp()
-        {
-            typeof(StartupTaskRunner).TypeInitializer.Invoke(null, new object[0]);
-        }
-
-        #endregion
-
         #region Tests
 
         [Test]
         public void Should_Configure_ControllerFactory()
         {
+            // arrange
+            var container = new Mock<IWindsorContainer>();
+
+            container.Setup(c => c.ResolveAll(typeof(IStartupTask))).Returns(new IStartupTask[] { });
+
+            // act
+            StartupTaskRunner.Run(container.Object);
+
+            // assert
             Assert.That(ControllerBuilder.Current.GetControllerFactory(), Is.TypeOf<WindsorControllerFactory>());
+        }
+
+        [Test]
+        public void Should_Reset_ControllerFactory()
+        {
+            // arrange
+            var serviceLocator = new Mock<IServiceLocator>();
+
+            serviceLocator.Setup(sl => sl.GetAllInstances<IStartupTask>()).Returns(new IStartupTask[] { });
+
+            ServiceLocator.SetLocatorProvider(() => serviceLocator.Object);
+
+            // act
+            StartupTaskRunner.Reset();
+
+            // assert
+            Assert.That(ControllerBuilder.Current.GetControllerFactory(), Is.TypeOf<DefaultControllerFactory>());
+        }
+
+        [Test]
+        public void Should_Reset_ServiceLocator()
+        {
+            // arrange
+            var serviceLocator = new Mock<IServiceLocator>();
+
+            serviceLocator.Setup(sl => sl.GetAllInstances<IStartupTask>()).Returns(new IStartupTask[] { });
+
+            ServiceLocator.SetLocatorProvider(() => serviceLocator.Object);
+
+            // act
+            StartupTaskRunner.Reset();
+
+            // assert
+            Assert.That(ServiceLocator.Current, Is.Null);
         }
 
         [Test]
         public void Should_Configure_ServiceLocator()
         {
+            // arrange
+            var container = new Mock<IWindsorContainer>();
+
+            container.Setup(c => c.ResolveAll(typeof(IStartupTask))).Returns(new IStartupTask[] { });
+
+            // act
+            StartupTaskRunner.Run(container.Object);
+
+            // assert
             Assert.That(ServiceLocator.Current, Is.TypeOf<WindsorServiceLocator>());
         }
 
         [Test]
-        public void Should_Run_And_Reset_StartupTasks()
+        public void Should_Execute_StartupTasks()
         {
-            TestDelegate condition = () =>
-                                         {
-                                             StartupTaskRunner.Run();
-                                             StartupTaskRunner.Reset();
-                                         };
+            // arrange
+            var container = new Mock<IWindsorContainer>();
+            var startupTask = new Mock<IStartupTask>();
 
-            Assert.That(condition, Throws.Nothing);
+            container.Setup(c => c.ResolveAll(typeof(IStartupTask))).Returns(new[] { startupTask.Object, startupTask.Object });
+
+            // act
+            StartupTaskRunner.Run(container.Object);
+
+            // assert
+            startupTask.Verify(task => task.Execute(), Times.Exactly(2));
         }
 
         [Test]
-        public void Should_Run_And_Reset_StartupTasks_Multiple_Times()
+        public void Should_Reset_StartupTasks()
         {
-            TestDelegate condition = () =>
-                                         {
-                                             StartupTaskRunner.Run();
-                                             StartupTaskRunner.Reset();
+            // arrange
+            var serviceLocator = new Mock<IServiceLocator>();
+            var startupTask = new Mock<IStartupTask>();
 
-                                             StartupTaskRunner.Run();
-                                             StartupTaskRunner.Reset();
-                                         };
+            serviceLocator.Setup(sl => sl.GetAllInstances<IStartupTask>()).Returns(new[] { startupTask.Object, startupTask.Object });
 
-            Assert.That(condition, Throws.Nothing);
+            ServiceLocator.SetLocatorProvider(() => serviceLocator.Object);
+
+            // act
+            StartupTaskRunner.Reset();
+
+            // assert
+            startupTask.Verify(task => task.Reset(), Times.Exactly(2));
         }
 
         #endregion
