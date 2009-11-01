c5ff4e83d92da32b354711887e4286e5a8dd52a7
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-11-01 14:28:39 +0000

    added unit test for IndexModelToEmailMapCreator

---
+++ Code/Com.Prerit.Web.Tests/Infrastructure/MapCreators/IndexModelToEmailMapCreatorTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+
+using Com.Prerit.Web.Infrastructure.MapCreators;
+
+using NUnit.Framework;
+
+namespace Com.Prerit.Web.Tests.Infrastructure.MapCreators
+{
+    [TestFixture]
+    public class IndexModelToEmailMapCreatorTests
+    {
+        #region Tests
+
+        [Test]
+        public void Should_Create_Map()
+        {
+            // arrange
+            Mapper.Reset();
+
+            // act
+            new IndexModelToEmailMapCreator().CreateMap();
+
+            // assert
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        #endregion
+    }
+}
