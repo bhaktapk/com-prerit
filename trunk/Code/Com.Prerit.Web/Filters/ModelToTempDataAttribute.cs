5925a72f70921a07fbfa09dde57926552b43924b
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-11-03 00:21:34 +0000

    removed requirement to only set TempData with the model on redirects

--- trunk/Code/Com.Prerit.Web/Filters/ModelToTempDataAttribute.cs
+++ trunk/Code/Com.Prerit.Web/Filters/ModelToTempDataAttribute.cs
@@ -14,13 +14,11 @@ namespace Com.Prerit.Web.Filters
 
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            ActionResult result = filterContext.Result;
-
             ControllerBase controller = filterContext.Controller;
 
             object model = filterContext.Controller.ViewData.Model;
 
-            if (model != null && result is RedirectToRouteResult || result is RedirectResult)
+            if (model != null)
             {
                 controller.TempData[TempDataKey] = model;
             }
