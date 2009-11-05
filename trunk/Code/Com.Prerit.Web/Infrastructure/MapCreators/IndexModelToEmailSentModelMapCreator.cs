1399796f09c41dd9f743dd35396cfdca27e234b1
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-11-05 00:49:15 +0000

    changed conversion of IndexModel to EmailSentModel from constructor to map
    added unit tests

---
+++ trunk/Code/Com.Prerit.Web/Infrastructure/MapCreators/IndexModelToEmailSentModelMapCreator.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+
+using Com.Prerit.Domain;
+using Com.Prerit.Web.Models.Contact;
+
+namespace Com.Prerit.Web.Infrastructure.MapCreators
+{
+    public class IndexModelToEmailSentModelMapCreator : IMapCreator
+    {
+        #region Methods
+
+        public void CreateMap()
+        {
+            Mapper.CreateMap<IndexModel, EmailSentModel>();
+        }
+
+        #endregion
+    }
+}
--- trunk/Code/Com.Prerit.Web/Models/Contact/EmailSentModel.cs
+++ trunk/Code/Com.Prerit.Web/Models/Contact/EmailSentModel.cs
@@ -6,22 +6,11 @@ namespace Com.Prerit.Web.Models.Contact
     {
         #region Properties
 
-        public string EmailAddress { get; private set; }
+        public string EmailAddress { get; set; }
 
-        public string Message { get; private set; }
+        public string Message { get; set; }
 
-        public string Name { get; private set; }
-
-        #endregion
-
-        #region Constructors
-
-        public EmailSentModel(IndexModel indexModel)
-        {
-            EmailAddress = indexModel.EmailAddress;
-            Message = indexModel.Message;
-            Name = indexModel.Name;
-        }
+        public string Name { get; set; }
 
         #endregion
     }
