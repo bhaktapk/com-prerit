6e7c09e58f61174e2b57850795892036b9ed5f56
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-11-07 15:37:33 +0000

    fixed bug where email sender and recipient where transposed

--- trunk/Code/Com.Prerit.Web/Infrastructure/MapCreators/IndexModelToEmailMapCreator.cs
+++ trunk/Code/Com.Prerit.Web/Infrastructure/MapCreators/IndexModelToEmailMapCreator.cs
@@ -12,8 +12,8 @@ namespace Com.Prerit.Web.Infrastructure.MapCreators
         public void CreateMap()
         {
             Mapper.CreateMap<IndexModel, Email>()
-                .ForMember(d => d.FromEmailAddress, opt => opt.MapFrom(s => EmailInfo.AuthorEmailAddress))
-                .ForMember(d => d.ToEmailAddress, opt => opt.MapFrom(s => s.EmailAddress))
+                .ForMember(d => d.FromEmailAddress, opt => opt.MapFrom(s => s.EmailAddress))
+                .ForMember(d => d.ToEmailAddress, opt => opt.MapFrom(s => EmailInfo.AuthorEmailAddress))
                 .ForMember(d => d.Subject, opt => opt.MapFrom(s => EmailInfo.GetContactEmailSubject(s.Name)));
         }
 
