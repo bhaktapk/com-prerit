79310063fe2cc35135b5bd1c86a355f2e98968b5
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-11-29 23:15:21 +0000

    abstracting away DefaultMasterModel by using RenderAction on meta tags

---
+++ Code/Com.Prerit.Web/Models/Shared/MetaTagsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Com.Prerit.Web.Models.Shared
+{
+    public class MetaTagsModel
+    {
+        #region Properties
+
+        public string ContentEncoding { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string Culture { get; set; }
+
+        public int CurrentYear { get; set; }
+
+        #endregion
+    }
+}
