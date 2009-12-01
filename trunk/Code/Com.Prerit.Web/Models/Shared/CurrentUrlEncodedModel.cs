35b4b16489a38ded950d83c08d3c05636e2705a9
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-12-01 00:29:44 +0000

    abstracting away DefaultMasterModel by using RenderAction for current encoded url

---
+++ trunk/Code/Com.Prerit.Web/Models/Shared/CurrentUrlEncodedModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Com.Prerit.Web.Models.Shared
+{
+    public class CurrentUrlEncodedModel
+    {
+        #region Properties
+
+        public string CurrentUrlEncoded { get; set; }
+
+        #endregion
+    }
+}
