33bf7c11bd8d713033eb3eddf7a401b9681bcf58
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-12-02 02:44:38 +0000

    extracted partial creation into an extension method for consistency

--- trunk/Code/Com.Prerit.Web/Helpers/Contact/ValidationSummaryHelper.cs
+++ trunk/Code/Com.Prerit.Web/Helpers/Contact/ValidationSummaryHelper.cs
@@ -1,11 +1,23 @@
 using System;
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
 
 namespace Com.Prerit.Web.Helpers.Contact
 {
     public static class ValidationSummaryHelper
     {
+        #region Constants
+
+        private const string _validationSummaryPartialName = "validation-summary";
+
+        #endregion
+
         #region Methods
+
+        public static void RenderValidationSummary(this HtmlHelper helper)
+        {
+            helper.RenderPartial(_validationSummaryPartialName);
+        }
 
         public static void RepeatErrorMessages(this HtmlHelper helper, Action<ModelError> render)
         {
