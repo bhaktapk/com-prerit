0652637aaee93146c6dc4a6854261266662f9b74
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-12-05 19:37:47 +0000

    added empty projects as the first step to merge all the separate projects together

---
+++ trunk/Code/Com.Prerit/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly : AssemblyTitle("Com.Prerit")]
+[assembly : AssemblyProduct("Com.Prerit")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly : Guid("3a82837a-e8d5-44c8-9b7e-929c919020d5")]
