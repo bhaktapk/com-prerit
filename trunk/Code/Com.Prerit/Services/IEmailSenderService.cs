9962923e6d711ac70a8ef53bf265d1591c97430f
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-12-05 19:55:16 +0000

    merged Com.Prerit.Services and Com.Prerit.Services.Tests into Com.Prerit and Com.Prerit.Tests respectively

---
+++ trunk/Code/Com.Prerit/Services/IEmailSenderService.cs
@@ -0,0 +1,13 @@
+﻿using Com.Prerit.Domain;
+
+namespace Com.Prerit.Services
+{
+    public interface IEmailSenderService
+    {
+        #region Methods
+
+        void Send(Email email);
+
+        #endregion
+    }
+}
