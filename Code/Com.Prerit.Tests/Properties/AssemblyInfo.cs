2baad2f834392a4a96fc20693023c176c1c0ef60
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2009-12-05 19:37:47 +0000

    added empty projects as the first step to merge all the separate projects together

---
+++ Code/Com.Prerit.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly : AssemblyTitle("Com.Prerit.Tests")]
+[assembly : AssemblyProduct("Com.Prerit.Tests")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly : Guid("47ff6bda-dceb-4115-a035-0352245a9fd4")]
