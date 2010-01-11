a12b3164f07e3db620eff7a0d55eac363aa5f978
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-01-11 13:36:15 +0000

    replaced catch-all default route with a specific root route of known controllers in order to eliminate duplicate content served by different urls
    added ListConstraint in order to constrain route params with a known list of values

---
+++ Code/Com.Prerit/Infrastructure/Routing/ListConstraint.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Com.Prerit.Infrastructure.Routing
+{
+    public class ListConstraint : IRouteConstraint
+    {
+        #region Fields
+
+        private readonly string[] _list;
+
+        #endregion
+
+        #region Constructors
+
+        public ListConstraint(params string[] list)
+        {
+            _list = list;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (string.IsNullOrEmpty(parameterName))
+            {
+                throw new ArgumentNullException("parameterName");
+            }
+
+            if (values == null)
+            {
+                throw new ArgumentNullException("values");
+            }
+
+            object value;
+
+            if (values.TryGetValue(parameterName, out value) && value is string)
+            {
+                return _list.Any(item => string.Compare(item, (string) value, StringComparison.InvariantCultureIgnoreCase) == 0);
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
