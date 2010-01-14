0e173bbc04874557f3b85c2864b182b1ac661a18
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-01-14 13:04:33 +0000

    added XrdsLocationAttribute for OpenId authentication

---
+++ Code/Com.Prerit/Filters/XrdsLocationAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Com.Prerit.Filters
+{
+    public class XrdsLocationAttribute : ActionFilterAttribute
+    {
+        #region Fields
+
+        private readonly string _path;
+
+        #endregion
+
+        #region Constructors
+
+        public XrdsLocationAttribute(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException("path");
+            }
+
+            _path = path;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            filterContext.HttpContext.Response.AppendHeader("x-xrds-location", _path);
+
+            base.OnResultExecuted(filterContext);
+        }
+
+        #endregion
+    }
+}
