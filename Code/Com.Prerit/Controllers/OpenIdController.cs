83bb79a9041ea2d83d01f967adba9f7a7fc064f5
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-01-20 14:13:39 +0000

    added OpenIdService to wrap dotnetopenauth
    implemented CreateRequest and added HandleResponse method

--- Code/Com.Prerit/Controllers/OpenIdController.cs
+++ Code/Com.Prerit/Controllers/OpenIdController.cs
@@ -1,7 +1,13 @@
+using System;
 using System.Web.Mvc;
+using System.Web.Security;
 
+using Com.Prerit.Controllers.Services;
 using Com.Prerit.Filters;
 using Com.Prerit.Models.OpenId;
+
+using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OpenId.RelyingParty;
 
 using MvcContrib.Filters;
 
@@ -9,6 +15,26 @@ namespace Com.Prerit.Controllers
 {
     public partial class OpenIdController : Controller
     {
+        #region Fields
+
+        private readonly IOpenIdService _openIdService;
+
+        #endregion
+
+        #region Constructors
+
+        public OpenIdController(IOpenIdService openIdService)
+        {
+            if (openIdService == null)
+            {
+                throw new ArgumentNullException("openIdService");
+            }
+
+            _openIdService = openIdService;
+        }
+
+        #endregion
+
         #region Methods
 
         [AcceptVerbs(HttpVerbs.Post)]
@@ -21,9 +47,29 @@ namespace Com.Prerit.Controllers
                 return RedirectToAction(MVC.Accounts.Login(model.ReturnUrl));
             }
 
-            // TODO: create request
+            var baseUri = new Uri(Request.Url, "/");
+            var returnToUri = new Uri(baseUri, Url.Action(MVC.OpenId.HandleResponse(model.ReturnUrl)));
 
-            return !string.IsNullOrEmpty(model.ReturnUrl) ? (ActionResult) Redirect(model.ReturnUrl) : RedirectToAction(MVC.About.Index());
+            IAuthenticationRequest request = _openIdService.CreateAuthenticationRequest(model.OpenIdIdentifier, baseUri, returnToUri);
+
+            return request.RedirectingResponse.AsActionResult();
+        }
+
+        public virtual ActionResult HandleResponse(string returnUrl)
+        {
+            IAuthenticationResponse response = _openIdService.GetAuthenticationResponse();
+
+            switch (response.Status)
+            {
+                case AuthenticationStatus.Authenticated:
+                    FormsAuthentication.SetAuthCookie(response.ClaimedIdentifier, false);
+
+                    return !string.IsNullOrEmpty(returnUrl) ? (ActionResult) Redirect(returnUrl) : RedirectToAction(MVC.About.Index());
+                default:
+                    ModelState.AddModelError(response.Exception.Message, response.Exception.Message);
+
+                    return RedirectToAction(MVC.Accounts.Login(returnUrl));
+            }
         }
 
         public virtual ActionResult Xrds()
---
+++ Code/Com.Prerit/Controllers/Services/OpenIdService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using DotNetOpenAuth.OpenId;
+using DotNetOpenAuth.OpenId.RelyingParty;
+
+namespace Com.Prerit.Controllers.Services
+{
+    public class OpenIdService : IOpenIdService
+    {
+        #region Methods
+
+        public IAuthenticationRequest CreateAuthenticationRequest(string openIdIdentifier, Uri baseUri, Uri returnToUri)
+        {
+            IAuthenticationRequest request;
+
+            using (var openIdRelyingParty = new OpenIdRelyingParty())
+            {
+                request = openIdRelyingParty.CreateRequest(Identifier.Parse(openIdIdentifier), new Realm(baseUri), returnToUri);
+            }
+
+            return request;
+        }
+
+        public IAuthenticationResponse GetAuthenticationResponse()
+        {
+            using (var openid = new OpenIdRelyingParty())
+            {
+                return openid.GetResponse();
+            }
+        }
+
+        #endregion
+    }
+}
