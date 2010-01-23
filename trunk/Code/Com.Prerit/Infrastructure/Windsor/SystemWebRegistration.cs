df70077478f4ee3a5a8519c957363884748aa6d5
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-01-23 14:41:17 +0000

    added system web registration for windsor

---
+++ trunk/Code/Com.Prerit/Infrastructure/Windsor/SystemWebRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+
+namespace Com.Prerit.Infrastructure.Windsor
+{
+    public class SystemWebRegistration : IRegistration
+    {
+        public void Register(IKernel kernel)
+        {
+            kernel
+                .Register(Component.For<HttpContextBase>()
+                    .LifeStyle.PerWebRequest
+                    .UsingFactoryMethod(() => new HttpContextWrapper(HttpContext.Current)))
+                .Register(Component.For<HttpRequestBase>()
+                    .LifeStyle.PerWebRequest
+                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Request))
+                .Register(Component.For<HttpResponseBase>()
+                    .LifeStyle.PerWebRequest
+                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Response))
+                .Register(Component.For<HttpSessionStateBase>()
+                    .LifeStyle.PerWebRequest
+                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Session));
+        }
+    }
+}
