0ebdcec755aebefa6010cc494ac828bf3e93c9c6
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-02 23:31:55 +0000

    added attribute to authorize admin accounts

---
+++ Code/Com.Prerit/Filters/AuthorizeAdminAttribute.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Com.Prerit.Services;
+
+namespace Com.Prerit.Filters
+{
+    public class AuthorizeAdminAttribute : FilterAttribute, IAuthorizationFilter
+    {
+        #region Fields
+
+        private readonly IMembershipService _membershipService;
+
+        #endregion
+
+        #region Constructors
+
+        public AuthorizeAdminAttribute()
+        {
+            HttpContext httpContext = HttpContext.Current;
+
+            if (httpContext == null)
+            {
+                throw new InvalidOperationException("Cannot construct object without a current HttpContext");
+            }
+
+            var cacheService = new CacheService(httpContext.Cache);
+
+            var httpSessionState = new HttpSessionStateWrapper(httpContext.Session);
+
+            var sessionService = new SessionService(httpSessionState);
+
+            var httpServerUtility = new HttpServerUtilityWrapper(httpContext.Server);
+
+            _membershipService = new MembershipService(cacheService, sessionService, httpServerUtility);
+        }
+
+        public AuthorizeAdminAttribute(IMembershipService membershipService)
+        {
+            if (membershipService == null)
+            {
+                throw new ArgumentNullException("membershipService");
+            }
+
+            _membershipService = membershipService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected virtual bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException("httpContext");
+            }
+
+            if (!httpContext.User.Identity.IsAuthenticated)
+            {
+                return false;
+            }
+
+            return _membershipService.GetAdminAccounts().Any(a => a.ClaimedIdentifier == httpContext.User.Identity.Name);
+        }
+
+        private void CacheValidateHandler(HttpContext context, object data, ref HttpValidationStatus validationStatus)
+        {
+            validationStatus = OnCacheAuthorization(new HttpContextWrapper(context));
+        }
+
+        public virtual void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
+
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectResult(new UrlHelper(filterContext.RequestContext).Action(MVC.Accounts.LogIn(filterContext.HttpContext.Request.Url.PathAndQuery)));
+                return;
+            }
+
+            if (!AuthorizeCore(filterContext.HttpContext))
+            {
+                filterContext.Result = new RedirectResult(new UrlHelper(filterContext.RequestContext).Action(MVC.Accounts.Unauthorized(filterContext.HttpContext.Request.Url.PathAndQuery)));
+                return;
+            }
+
+            HttpCachePolicyBase cache = filterContext.HttpContext.Response.Cache;
+
+            cache.SetProxyMaxAge(new TimeSpan(0));
+            cache.AddValidationCallback(CacheValidateHandler, null);
+        }
+
+        protected virtual HttpValidationStatus OnCacheAuthorization(HttpContextWrapper httpContext)
+        {
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException("httpContext");
+            }
+
+            if (!AuthorizeCore(httpContext))
+            {
+                return HttpValidationStatus.IgnoreThisRequest;
+            }
+
+            return HttpValidationStatus.Valid;
+        }
+
+        #endregion
+    }
+}
