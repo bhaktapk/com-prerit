65da5e6f727b6f4fb8a576db989c5dc5d6152b29
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-14 16:04:22 +0000

    added argument checking
    added methods for getting and setting album and albums

--- trunk/Code/Com.Prerit/Services/CacheService.cs
+++ trunk/Code/Com.Prerit/Services/CacheService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Caching;
 
 using Com.Prerit.Domain;
@@ -8,6 +10,10 @@ namespace Com.Prerit.Services
     public class CacheService : ICacheService
     {
         #region Constants
+
+        private const string AlbumKeyBase = "Album-{0}";
+
+        private const string AlbumsKey = "Albums";
 
         private const string ProfileKeyBase = "Profile-{0}";
 
@@ -37,6 +43,11 @@ namespace Com.Prerit.Services
 
         #region Methods
 
+        private string CreateAlbumKey(string dirPath)
+        {
+            return string.Format(AlbumKeyBase, dirPath);
+        }
+
         private string CreateProfileKey(string id)
         {
             return string.Format(ProfileKeyBase, id);
@@ -47,8 +58,28 @@ namespace Com.Prerit.Services
             return string.Format(RoleKeyBase, Enum.GetName(typeof(RoleType), roleType));
         }
 
+        public Album GetAlbum(string dirPath)
+        {
+            return _cache[CreateAlbumKey(dirPath)] as Album;
+        }
+
+        public IEnumerable<Album> GetAlbums()
+        {
+            return _cache[AlbumsKey] as IEnumerable<Album>;
+        }
+
         public Profile GetProfile(string id)
         {
+            if (id == null)
+            {
+                throw new ArgumentNullException("id");
+            }
+
+            if (id == string.Empty)
+            {
+                throw new ArgumentException("Cannot be empty string", "id");
+            }
+
             return _cache[CreateProfileKey(id)] as Profile;
         }
 
@@ -57,13 +88,70 @@ namespace Com.Prerit.Services
             return _cache[CreateRoleKey(roleType)] as Role;
         }
 
+        public void SetAlbum(Album album, string dirPath)
+        {
+            if (album == null)
+            {
+                throw new ArgumentNullException("album");
+            }
+
+            if (dirPath == null)
+            {
+                throw new ArgumentNullException("dirPath");
+            }
+
+            _cache.Insert(CreateAlbumKey(dirPath), album, new CacheDependency(dirPath));
+        }
+
+        public void SetAlbums(IEnumerable<Album> albums, string rootAlbumDirPath, string[] albumDirPaths)
+        {
+            if (albums == null)
+            {
+                throw new ArgumentNullException("albums");
+            }
+
+            if (rootAlbumDirPath == null)
+            {
+                throw new ArgumentNullException("rootAlbumDirPath");
+            }
+
+            var diskDependencies = new[]
+                                       {
+                                           rootAlbumDirPath
+                                       };
+
+            string[] albumCachekeys = albumDirPaths != null ? albumDirPaths.Select(path => CreateAlbumKey(path)).ToArray() : null;
+
+            _cache.Insert(AlbumsKey, albums, new CacheDependency(diskDependencies, albumCachekeys));
+        }
+
         public void SetProfile(Profile profile, string filePath)
         {
+            if (profile == null)
+            {
+                throw new ArgumentNullException("profile");
+            }
+
+            if (filePath == null)
+            {
+                throw new ArgumentNullException("filePath");
+            }
+
             _cache.Insert(CreateProfileKey(profile.Id), profile, new CacheDependency(filePath));
         }
 
         public void SetRole(Role role, string filePath)
         {
+            if (role == null)
+            {
+                throw new ArgumentNullException("role");
+            }
+
+            if (filePath == null)
+            {
+                throw new ArgumentNullException("filePath");
+            }
+
             _cache.Insert(CreateRoleKey(role.Type), role, new CacheDependency(filePath));
         }
 
--- trunk/Code/Com.Prerit/Services/ICacheService.cs
+++ trunk/Code/Com.Prerit/Services/ICacheService.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using Com.Prerit.Domain;
 
 namespace Com.Prerit.Services
@@ -6,9 +8,17 @@ namespace Com.Prerit.Services
     {
         #region Methods
 
+        Album GetAlbum(string dirPath);
+
+        IEnumerable<Album> GetAlbums();
+
         Profile GetProfile(string id);
 
         Role GetRole(RoleType roleType);
+
+        void SetAlbum(Album album, string dirPath);
+
+        void SetAlbums(IEnumerable<Album> albums, string rootAlbumDirPath, string[] albumDirPaths);
 
         void SetProfile(Profile profile, string filePath);
 
