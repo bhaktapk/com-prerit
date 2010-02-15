9672c7248cffc2495bf752197f8ce89af1a8aa30
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-15 14:57:46 +0000

    implemented getting Albums by reading folders and files

--- Code/Com.Prerit/Services/AlbumService.cs
+++ Code/Com.Prerit/Services/AlbumService.cs
@@ -1,18 +1,106 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
+using Com.Prerit.Core;
 using Com.Prerit.Domain;
+
+using Links;
 
 namespace Com.Prerit.Services
 {
     public class AlbumService : IAlbumService
     {
+        #region Fields
+
+        private readonly ICacheService _cacheService;
+
+        private readonly IDiskInputOutputService _diskInputOutputService;
+
+        private static readonly object AlbumDictionarySyncRoot = new object();
+
+        private static readonly Dictionary<string, object> AlbumSyncRoots = new Dictionary<string, object>();
+
+        #endregion
+
+        #region Constructors
+
+        public AlbumService(ICacheService cacheService, IDiskInputOutputService diskInputOutputService)
+        {
+            if (cacheService == null)
+            {
+                throw new ArgumentNullException("cacheService");
+            }
+
+            if (diskInputOutputService == null)
+            {
+                throw new ArgumentNullException("diskInputOutputService");
+            }
+
+            _cacheService = cacheService;
+            _diskInputOutputService = diskInputOutputService;
+        }
+
+        #endregion
+
         #region Methods
+
+        private Album GetAlbum(string dirPath)
+        {
+            if (_cacheService.GetAlbum(dirPath) == null)
+            {
+                lock (GetAlbumSyncRoot(dirPath))
+                {
+                    if (_cacheService.GetAlbum(dirPath) == null)
+                    {
+                        string albumFilePath = GetAlbumFilePath(dirPath);
+
+                        var album = _diskInputOutputService.LoadXmlFile<Album>(albumFilePath);
+
+                        album.PhotoCount = _diskInputOutputService.GetFiles(dirPath, "*.jpg", SearchOption.TopDirectoryOnly).Count();
+
+                        _cacheService.SetAlbum(album, dirPath);
+                    }
+                }
+            }
+
+            return _cacheService.GetAlbum(dirPath);
+        }
+
+        private string GetAlbumFilePath(string dirPath)
+        {
+            return Path.Combine(dirPath, "Album.xml");
+        }
+
+        private IEnumerable<Album> GetAlbumObjects(IEnumerable<string> validAlbumDirPaths)
+        {
+            return (from albumDirPath in validAlbumDirPaths
+                    select GetAlbum(albumDirPath)).ExecuteQuery();
+        }
 
         public IEnumerable<Album> GetAlbums()
         {
-            throw new NotImplementedException();
+            if (_cacheService.GetAlbums() == null)
+            {
+                lock (AlbumDictionarySyncRoot)
+                {
+                    if (_cacheService.GetAlbums() == null)
+                    {
+                        string albumRootDirPath = _diskInputOutputService.MapPath(App_Data.Albums.Url());
+
+                        IEnumerable<string> albumDirPaths = _diskInputOutputService.GetDirectories(albumRootDirPath);
+
+                        IEnumerable<string> validAlbumDirPaths = GetValidAlbumDirPaths(albumDirPaths);
+
+                        IEnumerable<Album> albums = GetAlbumObjects(validAlbumDirPaths);
+
+                        _cacheService.SetAlbums(albums, albumRootDirPath, albumDirPaths, validAlbumDirPaths);
+                    }
+                }
+            }
+
+            return _cacheService.GetAlbums();
         }
 
         public IEnumerable<Album> GetAlbums(int year)
@@ -36,11 +124,34 @@ namespace Com.Prerit.Services
             return albums.Select(album => album.Slug);
         }
 
+        private object GetAlbumSyncRoot(string dirPath)
+        {
+            if (!AlbumSyncRoots.ContainsKey(dirPath))
+            {
+                lock (AlbumDictionarySyncRoot)
+                {
+                    if (!AlbumSyncRoots.ContainsKey(dirPath))
+                    {
+                        AlbumSyncRoots.Add(dirPath, new object());
+                    }
+                }
+            }
+
+            return AlbumSyncRoots[dirPath];
+        }
+
         public IEnumerable<int> GetAlbumYears()
         {
             IEnumerable<Album> albums = GetAlbums();
 
             return albums.Select(album => album.Year).Distinct();
+        }
+
+        private IEnumerable<string> GetValidAlbumDirPaths(IEnumerable<string> albumDirPaths)
+        {
+            return (from albumDirPath in albumDirPaths
+                    where _diskInputOutputService.FileExists(GetAlbumFilePath(albumDirPath))
+                    select albumDirPath).ExecuteQuery();
         }
 
         #endregion
