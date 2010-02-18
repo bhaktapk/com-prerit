0b78ac23f895990fbe43af5364cb9ba08cea5623
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-18 00:34:41 +0000

    added stub for AllAlbumsModel and corresponding view

---
+++ Code/Com.Prerit/Models/Albums/AllAlbumsModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using Com.Prerit.Domain;
+
+namespace Com.Prerit.Models.Albums
+{
+    public class AllAlbumsModel
+    {
+        #region Fields
+
+        private IEnumerable<Album> _albums;
+
+        #endregion
+
+        #region Properties
+
+        public IEnumerable<Album> Albums
+        {
+            get { return _albums; }
+            set { _albums = value ?? new Album[0]; }
+        }
+
+        #endregion
+    }
+}
