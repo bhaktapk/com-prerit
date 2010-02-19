8757028065855a11a47ac5e4be0455d537df96ac
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-19 21:12:42 +0000

    added helper method to render album portraits
    implemented AlbumsByYear view

--- trunk/Code/Com.Prerit/Helpers/Albums/AlbumsHelper.cs
+++ trunk/Code/Com.Prerit/Helpers/Albums/AlbumsHelper.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 
+using Com.Prerit.Domain;
 using Com.Prerit.Models.Albums;
 
 namespace Com.Prerit.Helpers.Albums
@@ -9,6 +10,25 @@ namespace Com.Prerit.Helpers.Albums
     public static class AlbumsHelper
     {
         #region Methods
+
+        public static void RenderAlbumPortraitPartial(this HtmlHelper<AlbumsByYearModel> helper)
+        {
+            if (helper == null)
+            {
+                throw new ArgumentNullException("helper");
+            }
+
+            foreach (Album album in helper.ViewData.Model.Albums)
+            {
+                var model = new AlbumPortraitModel
+                                {
+                                    Album = album,
+                                    AlbumItemCount = null
+                                };
+
+                helper.RenderPartial(MVC.Albums.Views.AlbumPortrait, model);
+            }
+        }
 
         public static void RenderAlbumsByYearPartial(this HtmlHelper<AllAlbumsModel> helper)
         {
--- trunk/Code/Com.Prerit/Views/Albums/AlbumsByYear.ascx
+++ trunk/Code/Com.Prerit/Views/Albums/AlbumsByYear.ascx
@@ -1,3 +1,8 @@
+<%@ Import Namespace="Com.Prerit.Helpers.Albums" %>
 <%@ Import Namespace="Com.Prerit.Models.Albums" %>
 <%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<AlbumsByYearModel>" %>
 
+<h1><span><%= Model.Year %> Albums</span></h1>
+<div class="albums">
+    <% Html.RenderAlbumPortraitPartial(); %>
+</div>
