c5078cd3f53100d0777e5abd67aff37bff63c51b
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-19 21:10:38 +0000

    added stubs for AlbumPortrait and corresponding model

---
+++ Code/Com.Prerit/Models/Albums/AlbumPortraitModel.cs
@@ -0,0 +1,15 @@
+﻿using Com.Prerit.Domain;
+
+namespace Com.Prerit.Models.Albums
+{
+    public class AlbumPortraitModel
+    {
+        #region Properties
+
+        public Album Album { get; set; }
+
+        public int? AlbumItemCount { get; set; }
+
+        #endregion
+    }
+}
