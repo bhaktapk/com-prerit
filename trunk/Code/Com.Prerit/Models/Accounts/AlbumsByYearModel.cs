d0ff18d16ca5ae44e1bcf507f5774c5527290027
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-19 20:18:27 +0000

    added albums by year model

---
+++ trunk/Code/Com.Prerit/Models/Accounts/AlbumsByYearModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using Com.Prerit.Domain;
+
+namespace Com.Prerit.Models.Accounts
+{
+    public class AlbumsByYearModel
+    {
+        #region Properties
+
+        public IEnumerable<Album> Albums { get; set; }
+
+        public int Year { get; set; }
+
+        #endregion
+    }
+}
