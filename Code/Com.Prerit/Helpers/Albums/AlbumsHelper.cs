a6e2dd4e0e85d69bf8196a807b4b6dec1bd82186
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-19 20:49:14 +0000

    added helper to render albums by year partials

---
+++ Code/Com.Prerit/Helpers/Albums/AlbumsHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+using Com.Prerit.Models.Albums;
+
+namespace Com.Prerit.Helpers.Albums
+{
+    public static class AlbumsHelper
+    {
+        #region Methods
+
+        public static void RenderAlbumsByYearPartial(this HtmlHelper<AllAlbumsModel> helper)
+        {
+            if (helper == null)
+            {
+                throw new ArgumentNullException("helper");
+            }
+
+            foreach (var groupedAlbum in helper.ViewData.Model.GroupedAlbums)
+            {
+                var model = new AlbumsByYearModel
+                                {
+                                    Albums = groupedAlbum,
+                                    Year = groupedAlbum.Key
+                                };
+
+                helper.RenderPartial(MVC.Albums.Views.AlbumsByYear, model);
+            }
+        }
+
+        #endregion
+    }
+}
