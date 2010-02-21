d9a5334f4770724828b2c18aab6585bc91be7b61
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-21 01:12:03 +0000

    extended FilePathResult to mimic IIS' static file handling

---
+++ Code/Com.Prerit/ActionResults/StaticFilePathResult.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Com.Prerit.ActionResults
+{
+    public class StaticFilePathResult : FilePathResult
+    {
+        #region Properties
+
+        public HttpCacheability Cacheability { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public StaticFilePathResult(string fileName, string contentType, HttpCacheability cacheability)
+            : base(fileName, contentType)
+        {
+            Cacheability = cacheability;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            base.ExecuteResult(context);
+
+            string filePath = FileName.StartsWith("/") || FileName.StartsWith("~/") ? context.HttpContext.Server.MapPath(FileName) : FileName;
+
+            context.HttpContext.Response.AppendHeader("Accept-Ranges", "bytes");
+            context.HttpContext.Response.AddFileDependency(filePath);
+            context.HttpContext.Response.Cache.SetLastModified(File.GetLastWriteTime(filePath));
+            context.HttpContext.Response.Cache.SetETagFromFileDependencies();
+            context.HttpContext.Response.Cache.SetCacheability(Cacheability);
+            context.HttpContext.Response.Cache.SetOmitVaryStar(true);
+        }
+
+        #endregion
+    }
+}
