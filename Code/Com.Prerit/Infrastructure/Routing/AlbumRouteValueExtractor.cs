c737367d3feeb10b9eb5f164175e10bc3f02dfe8
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-02-23 14:55:33 +0000

    abstracted getting album route values to new AlbumRouteValueExtractor class

---
+++ Code/Com.Prerit/Infrastructure/Routing/AlbumRouteValueExtractor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Web.Routing;
+
+namespace Com.Prerit.Infrastructure.Routing
+{
+    public class AlbumRouteValueExtractor
+    {
+        #region Fields
+
+        private readonly RouteValueDictionary _values;
+
+        #endregion
+
+        #region Constructors
+
+        public AlbumRouteValueExtractor(RouteValueDictionary values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException("values");
+            }
+
+            _values = values;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool TryExtractSlug(string slugParameterName, out string slug)
+        {
+            object slugParameterValue;
+
+            if (_values.TryGetValue(slugParameterName, out slugParameterValue) && slugParameterValue is string)
+            {
+                slug = (string) slugParameterValue;
+
+                return true;
+            }
+
+            slug = null;
+
+            return false;
+        }
+
+        public bool TryExtractYear(string yearParameterName, out int year)
+        {
+            object yearParameterValue;
+
+            if (_values.TryGetValue(yearParameterName, out yearParameterValue))
+            {
+                if (yearParameterValue is int)
+                {
+                    year = (int) yearParameterValue;
+
+                    return true;
+                }
+
+                if (yearParameterValue is string && int.TryParse((string) yearParameterValue, out year))
+                {
+                    return true;
+                }
+            }
+
+            year = 0;
+
+            return false;
+        }
+
+        #endregion
+    }
+}
