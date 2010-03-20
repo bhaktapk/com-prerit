760147e87131dd20e1daefa11ef13037ede73ec7
prerit.bhakta <prerit.bhakta@5e21b246-8749-0410-827b-d3b12f9a86e0>
2010-03-20 12:24:09 +0000

    added Windsor registration for AutoMapper

---
+++ Code/Com.Prerit/Infrastructure/Windsor/AutoMapperRegistration.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+
+namespace Com.Prerit.Infrastructure.Windsor
+{
+    public class AutoMapperRegistration : IRegistration
+    {
+        #region Methods
+
+        public void Register(IKernel kernel)
+        {
+            const string assemblyName = "AutoMapper";
+
+            kernel
+                .Register(Component.For<IMappingEngine>()
+                    .UsingFactoryMethod(() => Mapper.Engine));
+        }
+
+        #endregion
+    }
+}
