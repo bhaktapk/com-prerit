d5cd2b7f9674fb700aa82e143300468a2cf8dfd9
prerit.bhakta@gmail.com <prerit.bhakta@gmail.com@5e21b246-8749-0410-827b-d3b12f9a86e0>
2011-03-09 14:01:59 +0000

    removed errant usage of LifeStyle Transient for AutoMapper classes
    simplified AutoMapper registration to only classes needed by removing IConfiguration, IFormatterExpression and IProfileConfiguration
    
    updated comment indicating that there is no way to remove maps when Reset is called from AutoMapperConfigurationStartupTask

--- trunk/Code/Com.Prerit.Tests/Infrastructure/Windsor/AutoMapperRegistrationTests.cs
+++ trunk/Code/Com.Prerit.Tests/Infrastructure/Windsor/AutoMapperRegistrationTests.cs
@@ -1,5 +1,8 @@
-﻿using AutoMapper;
+﻿using System.Collections.Generic;
 
+using AutoMapper;
+
+using Castle.MicroKernel;
 using Castle.Windsor;
 
 using Com.Prerit.Infrastructure.Windsor;
@@ -14,38 +17,6 @@ namespace Com.Prerit.Tests.Infrastructure.Windsor
         #region Tests
 
         [Test]
-        public void Should_Register_Transient_IConfiguration()
-        {
-            // arrange
-            var container = new WindsorContainer();
-
-            // act
-            container.Register(new AutoMapperRegistration());
-
-            var configuration1 = container.Resolve<IConfiguration>();
-            var configuration2 = container.Resolve<IConfiguration>();
-
-            // assert
-            Assert.That(configuration1, Is.Not.EqualTo(configuration2));
-        }
-
-        [Test]
-        public void Should_Register_Transient_IMappingEngine()
-        {
-            // arrange
-            var container = new WindsorContainer();
-
-            // act
-            container.Register(new AutoMapperRegistration());
-
-            var mappingEngine1 = container.Resolve<IMappingEngine>();
-            var mappingEngine2 = container.Resolve<IMappingEngine>();
-
-            // assert
-            Assert.That(mappingEngine1, Is.Not.EqualTo(mappingEngine2));
-        }
-
-        [Test]
         public void Should_Not_Use_Static_Mapper_Class()
         {
             // arrange
@@ -58,6 +29,73 @@ namespace Com.Prerit.Tests.Infrastructure.Windsor
 
             // assert
             Assert.That(mappingEngine, Is.Not.EqualTo(Mapper.Engine));
+        }
+
+        [Test]
+        public void Should_Register_IConfigurationProvider()
+        {
+            // arrange
+            var container = new WindsorContainer();
+            var handlers = new List<IHandler>();
+
+            container.Kernel.HandlerRegistered += (IHandler handler, ref bool stateChanged) =>
+                                                      {
+                                                          if (handler.Service == typeof(IConfigurationProvider))
+                                                          {
+                                                              handlers.Add(handler);
+                                                          }
+                                                      };
+
+            // act
+            container.Register(new AutoMapperRegistration());
+
+            // assert
+            Assert.That(handlers, Is.Not.Null.And.Not.Empty);
+        }
+
+        [Test]
+        public void Should_Register_IMappingEngine()
+        {
+            // arrange
+            var container = new WindsorContainer();
+
+            var handlers = new List<IHandler>();
+
+            container.Kernel.HandlerRegistered += (IHandler handler, ref bool stateChanged) =>
+                                                      {
+                                                          if (handler.Service == typeof(IMappingEngine))
+                                                          {
+                                                              handlers.Add(handler);
+                                                          }
+                                                      };
+
+            // act
+            container.Register(new AutoMapperRegistration());
+
+            // assert
+            Assert.That(handlers, Is.Not.Null.And.Not.Empty);
+        }
+
+        [Test]
+        public void Should_Register_IProfileExpression()
+        {
+            // arrange
+            var container = new WindsorContainer();
+            var handlers = new List<IHandler>();
+
+            container.Kernel.HandlerRegistered += (IHandler handler, ref bool stateChanged) =>
+                                                      {
+                                                          if (handler.Service == typeof(IProfileExpression))
+                                                          {
+                                                              handlers.Add(handler);
+                                                          }
+                                                      };
+
+            // act
+            container.Register(new AutoMapperRegistration());
+
+            // assert
+            Assert.That(handlers, Is.Not.Null.And.Not.Empty);
         }
 
         #endregion
