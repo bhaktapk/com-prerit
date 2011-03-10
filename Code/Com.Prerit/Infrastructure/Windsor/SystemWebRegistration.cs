e87acf56458df5571ae8ecd9de8f68d538b646de
prerit.bhakta <prerit.bhakta@gmail.com>
2011-03-10 13:50:11 +0000

    reorganized code

--- Code/Com.Prerit/Infrastructure/Windsor/SystemWebRegistration.cs
+++ Code/Com.Prerit/Infrastructure/Windsor/SystemWebRegistration.cs
@@ -16,26 +16,74 @@ namespace Com.Prerit.Infrastructure.Windsor
         {
             AddFacility<FactorySupportFacility>(kernel);
 
-            kernel
-                .Register(Component.For<Cache>()
-                    .UsingFactoryMethod(() => HttpRuntime.Cache))
-                .Register(Component.For<HttpContextBase>()
+            RegisterCache(kernel);
+            RegisterHttpContextBase(kernel);
+            RegisterHttpRequestBase(kernel);
+            RegisterHttpResponseBase(kernel);
+            RegisterHttpSessionStateBase(kernel);
+            RegisterHttpServerUtilityBase(kernel);
+            RegisterRouteCollection(kernel);
+        }
+
+        private void RegisterCache(IKernel kernel)
+        {
+            kernel.Register(
+                Component.For<Cache>()
+                    .UsingFactoryMethod(() => HttpRuntime.Cache)
+            );
+        }
+
+        private void RegisterHttpContextBase(IKernel kernel)
+        {
+            kernel.Register(
+                Component.For<HttpContextBase>()
                     .LifeStyle.PerWebRequest
-                    .UsingFactoryMethod(() => new HttpContextWrapper(HttpContext.Current)))
-                .Register(Component.For<HttpRequestBase>()
+                    .UsingFactoryMethod(() => new HttpContextWrapper(HttpContext.Current))
+            );
+        }
+
+        private void RegisterHttpRequestBase(IKernel kernel)
+        {
+            kernel.Register(
+                Component.For<HttpRequestBase>()
                     .LifeStyle.PerWebRequest
-                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Request))
-                .Register(Component.For<HttpResponseBase>()
+                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Request)
+            );
+        }
+
+        private void RegisterHttpResponseBase(IKernel kernel)
+        {
+            kernel.Register(
+                Component.For<HttpResponseBase>()
                     .LifeStyle.PerWebRequest
-                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Response))
-                .Register(Component.For<HttpSessionStateBase>()
+                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Response)
+            );
+        }
+
+        private void RegisterHttpServerUtilityBase(IKernel kernel)
+        {
+            kernel.Register(
+                Component.For<HttpServerUtilityBase>()
                     .LifeStyle.PerWebRequest
-                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Session))
-                .Register(Component.For<HttpServerUtilityBase>()
+                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Server)
+            );
+        }
+
+        private void RegisterHttpSessionStateBase(IKernel kernel)
+        {
+            kernel.Register(
+                Component.For<HttpSessionStateBase>()
                     .LifeStyle.PerWebRequest
-                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Server))
-                .Register(Component.For<RouteCollection>()
-                    .UsingFactoryMethod(() => RouteTable.Routes));
+                    .UsingFactoryMethod(k => k.Resolve<HttpContextBase>().Session)
+                );
+        }
+
+        private void RegisterRouteCollection(IKernel kernel)
+        {
+            kernel.Register(
+                Component.For<RouteCollection>()
+                    .UsingFactoryMethod(() => RouteTable.Routes)
+            );
         }
 
         #endregion
